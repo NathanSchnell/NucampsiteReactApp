b0d2441af64677e706a947ca93eef6868e35b742
Nate <schnell.nate@gmail.com>
2023-08-10 20:49:34 -0400

    updated features with new pages

---
+++ src/pages/HomePage.js
@@ -0,0 +1,11 @@
+import { Container } from "reactstrap";
+import DisplayList from "../features/display/DisplayList";
+
+
+const HomePage = () => {
+    return (
+        <DisplayList />
+    );
+};
+
+export default HomePage;
