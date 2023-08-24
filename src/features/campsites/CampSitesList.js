272f1fa4c3ae07d8d32f7bf4d2683e3f2986ecf8
Nomad <schnell.nate@gmail.com>
2023-08-24 19:39:33 -0400

    Campsites useSelector

--- src/features/campsites/CampSitesList.js
+++ src/features/campsites/CampSitesList.js
@@ -1,9 +1,13 @@
+import { useSelector } from 'react-redux';
 import { Col, Row, } from 'reactstrap';
 import CampsiteCard from "./CampsiteCard";
 import { selectAllCampsites } from './campsitesSlice';
 
 const CampsitesList = () => {
-    const campsites = selectAllCampsites();
+    const campsites = useSelector(selectAllCampsites);
+    //Logs the campsites selector to the console for debugging purposes
+    console.log('campsites:', campsites);
+
     return (
         <Row className='ms-auto'>
             {campsites.map((campsite) => {
